3fe5d168f93b2d8431c42c5322e43d1859607d0a
Alberto Leon <albertoleon325@gmail.com>
2018-09-13 10:04:04 -0700

    Assignment2 project structure

---
+++ assignment2/assignment2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  assignment2
+//
+//  Created by Alberto Leon on 9/13/18.
+//  Copyright © 2018 Alberto Leon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
